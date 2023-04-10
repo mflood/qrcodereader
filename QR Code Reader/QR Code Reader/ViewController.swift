b3bc858467c8626890fba58f8973766152f79b91
mflood <hermannwest@gmail.com>
2023-04-10 06:51:51 -0600

    Init project

---
+++ QR Code Reader/QR Code Reader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QR Code Reader
+//
+//  Created by Matthew Flood on 4/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
