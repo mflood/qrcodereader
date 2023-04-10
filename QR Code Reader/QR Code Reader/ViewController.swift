827beeb8f03bdc23d64bd43c66a5cbedaae1f497
mflood <hermannwest@gmail.com>
2023-04-10 06:53:09 -0600

    Add QR Code to view controller

--- QR Code Reader/QR Code Reader/ViewController.swift
+++ QR Code Reader/QR Code Reader/ViewController.swift
@@ -6,14 +6,53 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureMetadataOutputObjectsDelegate {
+    
+    var captureSession: AVCaptureSession!
+    var previewLayer: AVCaptureVideoPreviewLayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        // Set up the capture session
+        captureSession = AVCaptureSession()
+        
+        // Set up the camera input
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        guard let input = try? AVCaptureDeviceInput(device: captureDevice) else { return }
+        captureSession.addInput(input)
+        
+        // Set up the metadata output
+        let metadataOutput = AVCaptureMetadataOutput()
+        captureSession.addOutput(metadataOutput)
+        metadataOutput.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        metadataOutput.metadataObjectTypes = [.qr]
+        
+        // Set up the preview layer
+        previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        previewLayer.frame = view.layer.bounds
+        previewLayer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(previewLayer)
+        
+        // Start the capture session
+        captureSession.startRunning()
     }
-
-
+    
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        // Check if the metadata output contains a QR code
+        guard let metadataObject = metadataObjects.first as? AVMetadataMachineReadableCodeObject,
+              metadataObject.type == .qr,
+              let qrCodeValue = metadataObject.stringValue
+        else { return }
+        
+        // Display the QR code value in an alert
+        let alertController = UIAlertController(title: "QR Code Scanned", message: qrCodeValue, preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+        
+        // Stop the capture session
+        captureSession.stopRunning()
+    }
 }
-
